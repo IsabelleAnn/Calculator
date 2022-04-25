dc0da63969a19675789df091a9cf92c17bb51785
IsabelleAnn <isabelle.ann85@gmail.com>
2022-04-25 17:20:19 -0500

    create page layout and design, create operator functions and display functions

---
+++ script.js
@@ -0,0 +1,66 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function calculate(myFunc, a, b) {
+    return myFunc(a, b);
+}
+
+const displayBox = document.querySelector(".outcome-container");
+const defaultDisplay = displayBox.textContent;
+
+const numberBtns = document.querySelectorAll(".number");
+const decimalBtn = document.querySelector("#decimal");
+const clearBtn = document.querySelector("#clear");
+const operatorBtns = document.querySelectorAll(".operator");
+const equalsBtn = document.querySelector("#equals");
+
+let firstNumber = "";
+
+numberBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if (displayBox.textContent === defaultDisplay) {
+            displayBox.textContent = "";
+        }
+        displayBox.textContent += btn.textContent;
+        firstNumber += btn.textContent;
+        console.log(firstNumber);
+    });
+});
+
+decimalBtn.addEventListener("click", () => {
+    displayBox.textContent += decimalBtn.textContent;
+    firstNumber += decimalBtn.textContent;
+}, { once: true });
+
+clearBtn.addEventListener("click", () => {
+    displayBox.textContent = "";
+    firstNumber = "";
+});
+
+operatorBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+    });
+});
+
+
+//1.click number, display number
+//2.allow 1 decimal input
+//3.save number as firstNumber
+//4.click operation, clear display
+//5.get second number input
+//6.click =
+//7.execute operator function and display result
