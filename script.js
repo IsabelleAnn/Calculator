8a2098f2cfb884fab72234e03bc8f780e4d9c996
IsabelleAnn <isabelle.ann85@gmail.com>
2022-05-04 16:38:15 -0500

    edit numberBtns function

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (num == 0) {
+        return "Can't do that."
+    }
     return a / b;
 }
 
-function moveDecimalToLeft(a) {
+function percentToDecimal(a) {
     return (Math.round(a * 100) / 100) / 100;
 }
 
@@ -27,52 +30,79 @@ function calculate(myFunc, a, b) {
 }
 
 const displayBox = document.querySelector(".outcome-container");
-const defaultDisplay = displayBox.textContent;
 
 const numberBtns = document.querySelectorAll(".number");
 const decimalBtn = document.querySelector("#decimal");
 const clearBtn = document.querySelector("#clear");
 const deleteBtn = document.querySelector("#delete");
+const percentBtn = document.querySelector("#percent");
 const operatorBtns = document.querySelectorAll(".operator");
 const equalsBtn = document.querySelector("#equals");
 
-let firstNumber = "";
+let prevInput = "";
+let nextInput = "";
+let decimalPressed = false;
+let operatorPressed = false;
 
 numberBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        if (displayBox.textContent === defaultDisplay) {
-            displayBox.textContent = "";
+        if (operatorPressed) {
+            nextInput += btn.textContent;
+            displayBox.textContent = nextInput;
+
+        } else {
+            prevInput += btn.textContent;
+            displayBox.textContent = prevInput;
         }
-        displayBox.textContent += btn.textContent;
-        firstNumber += btn.textContent;
-        console.log(firstNumber);
+
+        console.log("prevInput", prevInput, "nextInput", nextInput);
     });
 });
 
 decimalBtn.addEventListener("click", () => {
-    displayBox.textContent += decimalBtn.textContent;
-    firstNumber += decimalBtn.textContent;
-}, { once: true });
+    if (!decimalPressed) {
+        displayBox.textContent += decimalBtn.textContent;
+        prevInput += decimalBtn.textContent;
+        decimalPressed = true;
+    }
+});
 
 clearBtn.addEventListener("click", () => {
     displayBox.textContent = "";
-    firstNumber = "";
+    prevInput = "";
+    decimalPressed = false;
 });
 
+let deleted = "";
 deleteBtn.addEventListener("click", () => {
+    deleted = prevInput.slice(-1);
+    prevInput = prevInput.substring(0, prevInput.length - 1);
 
+    displayBox.textContent = prevInput;
+    console.log("deleted", deleted);
+    if (deleted === ".") {
+        decimalPressed = false;
+
+    }
+});
+
+percentBtn.addEventListener("click", () => {
+    prevInput = percentToDecimal(prevInput);
+    prevInput = prevInput.toString();
+    displayBox.textContent = prevInput;
+    decimalPressed = true;
 });
 
 operatorBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-
+        operatorPressed = true;
     });
 });
 
 
 //1.click number, display number
 //2.allow 1 decimal input
-//3.save number as firstNumber
+//3.save number as prevInput
 //4.click operation, clear display
 //5.get second number input
 //6.click =
