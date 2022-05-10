19ae3e30cb09c9d078f3d31c07e175d69760e73b
IsabelleAnn <isabelle.ann85@gmail.com>
2022-05-09 20:36:42 -0500

    fix bugs and simplify methods

--- script.js
+++ script.js
@@ -19,8 +19,9 @@ function percentToDecimal(num) {
 }
 
 function round(num) {
-    console.log(num);
-    return parseFloat(num).toFixed(2);
+
+    let string = num.toString();
+    return parseFloat(num).toFixed(string.length - 1);
 }
 
 function calculate(myFunc, a, b) {
@@ -53,7 +54,7 @@ let prevInput = "";
 let currentInput = "";
 let decimalPressed = false;
 let operatorPressed = false;
-
+let equalsPressed = false;
 
 numberBtns.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -62,21 +63,17 @@ numberBtns.forEach(btn => {
             if (checkLength(currentInput.length)) {
                 displayBox.textContent = currentInput;
             }
-
         } else {
             prevInput += btn.textContent;
             if (checkLength(prevInput.length)) {
                 displayBox.textContent = prevInput;
             }
-
-
         }
-        console.log("prevInput", prevInput, "currentInput", currentInput);
     });
 });
 
 signBtn.addEventListener("click", () => {
-    if (operatorPressed && currentInput !== "") {
+    if (operatorPressed) {
         currentInput = switchSign(currentInput);
         displayBox.textContent = currentInput;
     } else {
@@ -90,13 +87,9 @@ decimalBtn.addEventListener("click", () => {
         if (operatorPressed) {
             if (currentInput === "") {
                 currentInput = "0";
-                currentInput += decimalBtn.textContent;
-
-            } else {
-                currentInput += decimalBtn.textContent;
             }
+            currentInput += decimalBtn.textContent;
             displayBox.textContent = currentInput;
-
         } else {
             if (prevInput === "") {
                 prevInput = "0";
@@ -106,7 +99,6 @@ decimalBtn.addEventListener("click", () => {
             }
             displayBox.textContent = prevInput;
         }
-
     }
     decimalPressed = true;
 });
@@ -118,20 +110,19 @@ clearBtn.addEventListener("click", () => {
     outcome = "";
     decimalPressed = false;
     operatorPressed = false;
+    equalsPressed = false;
 });
 
 let deleted = "";
 deleteBtn.addEventListener("click", () => {
-    if (currentInput !== "") {
-        deleted = currentInput.slice(-1);
+    if (operatorPressed) {
+        currentInput = currentInput.toString();
         currentInput = currentInput.substring(0, currentInput.length - 1);
         displayBox.textContent = currentInput;
-        console.log("deleted", deleted);
-    } else if (!operatorPressed) {
-        deleted = prevInput.slice(-1);
+    } else {
+        prevInput = prevInput.toString();
         prevInput = prevInput.substring(0, prevInput.length - 1);
         displayBox.textContent = prevInput;
-        console.log("deleted", deleted);
     }
     if (deleted === ".") {
         decimalPressed = false;
@@ -139,39 +130,43 @@ deleteBtn.addEventListener("click", () => {
 });
 
 percentBtn.addEventListener("click", () => {
-    if (currentInput !== "") {
+    if (operatorPressed) {
         currentInput = percentToDecimal(currentInput);
         currentInput = currentInput.toString();
         displayBox.textContent = currentInput;
-    } else if (!operatorPressed) {
+    } else {
         prevInput = percentToDecimal(prevInput);
-        prevInput = prevInput.toString();
-        displayBox.textContent = prevInput;
-    } else if (outcome !== "") {
-        prevInput = percentToDecimal(outcome);
         prevInput = prevInput.toString();
         displayBox.textContent = prevInput;
     }
     decimalPressed = true;
-
 });
 
 let chosenOperator;
 let outcome;
+
 operatorBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         operatorPressed = true;
         decimalPressed = false;
-        if (currentInput === "0" && chosenOperator === divide) {
-            currentInput = "";
+        equalsPressed = false;
+        if (parseInt(currentInput) === 0 && chosenOperator === divide) {
             displayBox.textContent = "Can't do that!";
+            currentInput = "";
+            prevInput = "";
+            outcome = "";
         }
-        if (currentInput !== "") {
+        if (currentInput !== "" && prevInput !== "") {
             outcome = Number(parseFloat(calculate(chosenOperator, Number(prevInput), Number(currentInput))));
             displayBox.textContent = outcome;
             currentInput = outcome;
         }
-
+        if (prevInput === "" && outcome !== "") {
+            currentInput = outcome;
+            outcome = Number(parseFloat(calculate(chosenOperator, Number(prevInput), Number(currentInput))));
+            displayBox.textContent = outcome;
+            currentInput = "";
+        }
         switch (btn.textContent) {
             case "x":
                 chosenOperator = multiply;
@@ -190,32 +185,26 @@ operatorBtns.forEach(btn => {
                 console.log("add", btn.textContent);
                 break;
         }
-
         if (currentInput !== "") {
             prevInput = currentInput;
             currentInput = "";
         }
-
-        console.log("prevInput", prevInput, "currentInput", currentInput, "outcome", outcome);
     });
 }, );
 
 equalsBtn.addEventListener("click", () => {
-    if (currentInput === "0" && chosenOperator === divide) {
-        currentInput = "";
+    equalsPressed = true;
+    operatorPressed = false;
+    if (parseInt(currentInput) === 0 && chosenOperator === divide) {
         displayBox.textContent = "Can't do that!";
+        currentInput = "";
+        prevInput = "";
+        outcome = "";
     }
     if (prevInput !== "" && currentInput !== "") {
         outcome = Number(parseFloat(calculate(chosenOperator, Number(prevInput), Number(currentInput))));
         displayBox.textContent = outcome;
-        prevInput = outcome;
     }
+    prevInput = outcome;
     currentInput = "";
 });
-//1.click number, display number
-//2.allow 1 decimal input
-//3.save number as prevInput
-//4.click operation, clear display
-//5.get second number input
-//6.click =
-//7.execute operator function and display resultclass
